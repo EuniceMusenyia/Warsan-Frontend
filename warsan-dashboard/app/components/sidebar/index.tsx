ae8374cafa2551a99b859e953c032922c23f62ea
kilas-Aniso <124134048+kilas-Aniso@users.noreply.github.com>
2023-09-19 20:15:27 +0300

    made changes on the sidebar code

--- warsan-dashboard/app/components/sidebar/index.tsx
+++ warsan-dashboard/app/components/sidebar/index.tsx
@@ -3,30 +3,34 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { RxPerson } from 'react-icons/rx';
-import { FiAlertCircle, FiHome, FiPauseCircle, FiUser, FiUserPlus, FiUsers  } from 'react-icons/fi';
+import {BiSolidUserCircle} from 'react-icons/bi';
+import { PiUsersFourFill } from 'react-icons/pi';
+import { HiHome } from 'react-icons/hi';
+import { RiAlertFill } from 'react-icons/ri';
+import { FaUsers } from 'react-icons/fa';
+import { IoLogOutSharp } from 'react-icons/io5';
 
 // Sidebar component
 export const Sidebar = ({ children }) => {
   return (
     <div className='flex h-screen fixed'>
-      <div className='flex flex-col w-30 md:w-30 p-2 sm:p-3 md:p-5 bg-blue-600 border-r-1 md:border-r-0'>
+      <div className='flex flex-col w-30 md:w-30 p-2 sm:p-3 md:p-5 bg-gray-100 border-r-2 md:border-r-1 border-blue-600 shadow-lg'>
         <div className='flex items-center mb-10'>
-          <img src={'./logo.png'} alt="arrow" className="md:w-14 md:h-14 w-5 h-5 object-cover -ml-2 mb-4 mt-2 " />
-          <span className='text-white md:text-2xl sm:text-sm font-bold mb-4 mt-2'>RAJO</span>
+          <img src={'./logo.png'} alt="arrow" className="md:w-14 md:h-14 w-5 h-5 object-cover -ml-2 mb-4 mt-2  " />
+          <span className='text-blue-600 md:text-2xl sm:text-sm font-bold mb-4 mt-2'>RAJO</span>
         </div>
-        <hr className='mb-5' />
-          
+        <hr className='mb-5' style={{ borderColor: 'blue' }} />
         <div className=''>
-          <NavItem icon={<FiHome className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Home" />
-          <NavItem icon={<FiAlertCircle className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Alerts" />
-          <NavItem icon={<FiUsers className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="CHV" />
-          <NavItem icon={<FiUserPlus className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Children" />
+          <NavItem icon={<HiHome className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Home" />
+          <NavItem icon={<RiAlertFill className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Alerts" />
+          <NavItem icon={<FaUsers className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="CHV" />
+          <NavItem icon={<PiUsersFourFill className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Children" />
         </div>
 
         <div className='mt-auto'>
-          <hr className='mb-5' />
-          <NavItem icon={<FiUser className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Profile" />
+        <hr className='mb-5' style={{ borderColor: 'blue' }} />
+          <NavItem icon={<BiSolidUserCircle className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Profile" />
+          <NavItem icon={<IoLogOutSharp className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Logout" />
         </div>
       </div>
       <main className='flex-1'>{children}</main>
@@ -37,24 +41,22 @@ export const Sidebar = ({ children }) => {
 // NavItem component
 const NavItem = ({ icon, label }) => {
   const handleMouseEnter = (event) => {
-    event.currentTarget.style.color = 'pink';
+    event.currentTarget.style.color = 'purple';
   };
 
   const handleMouseLeave = (event) => {
-    event.currentTarget.style.color = 'white';
+    event.currentTarget.style.color = 'blue';
   };
 
   return (
     <div
-      className='flex flex-col items-center text-white my-1 p-1 rounded-lg navitems'
+      className='flex items-center text-blue-600 my-1 p-1 rounded-lg navitems'
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <div className='flex items-center'>
-        {icon}
-      </div>
-      <div className='mt-1 mb-10'>
-        <span className='md:text-xl text-sm'>{label}</span>
+      <div className='flex items-end'>
+        <div className='mr-2 mb-10'>{icon}</div>
+        <span className='md:text-xl text-sm mb-10'>{label}</span>
       </div>
     </div>
   );
