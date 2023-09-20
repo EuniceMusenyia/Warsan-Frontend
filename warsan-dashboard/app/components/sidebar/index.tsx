d8649753222a49156f507f539d71ad6581c43074
kilas-Aniso <124134048+kilas-Aniso@users.noreply.github.com>
2023-09-20 15:04:49 +0300

    made changes on the sidebar

--- warsan-dashboard/app/components/sidebar/index.tsx
+++ warsan-dashboard/app/components/sidebar/index.tsx
@@ -1,63 +1,69 @@
-// Import statements
-'use client';
+'use client'
+import React, { ReactElement } from 'react';
+import { RiAccountCircleLine } from 'react-icons/ri';
+import { LuLayoutGrid } from 'react-icons/lu';
+import { TbAlertTriangle } from 'react-icons/tb';
+import { PiUsersThreeDuotone,PiUsersFour } from 'react-icons/pi';
+import { MdLogout } from 'react-icons/md';
 
-import React from 'react';
-import Link from 'next/link';
-import {BiSolidUserCircle} from 'react-icons/bi';
-import { PiUsersFourFill } from 'react-icons/pi';
-import { HiHome } from 'react-icons/hi';
-import { RiAlertFill } from 'react-icons/ri';
-import { FaUsers } from 'react-icons/fa';
-import { IoLogOutSharp } from 'react-icons/io5';
+interface ChildItem {
+  icon: ReactElement;
+  label: string;
+}
 
-// Sidebar component
-export const Sidebar = ({ children }) => {
-  return (
-    <div className='flex h-screen fixed'>
-      <div className='flex flex-col w-30 md:w-30 p-2 sm:p-3 md:p-5 bg-gray-100 border-r-2 md:border-r-1 border-blue-600 shadow-lg'>
-        <div className='flex items-center mb-10'>
-          <img src={'./logo.png'} alt="arrow" className="md:w-14 md:h-14 w-5 h-5 object-cover -ml-2 mb-4 mt-2  " />
-          <span className='text-blue-600 md:text-2xl sm:text-sm font-bold mb-4 mt-2'>RAJO</span>
-        </div>
-        <hr className='mb-5' style={{ borderColor: 'blue' }} />
-        <div className=''>
-          <NavItem icon={<HiHome className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Home" />
-          <NavItem icon={<RiAlertFill className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Alerts" />
-          <NavItem icon={<FaUsers className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="CHV" />
-          <NavItem icon={<PiUsersFourFill className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Children" />
-        </div>
+interface NavItemProps {
+  item: ChildItem;
+}
 
-        <div className='mt-auto'>
-        <hr className='mb-5' style={{ borderColor: 'blue' }} />
-          <NavItem icon={<BiSolidUserCircle className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Profile" />
-          <NavItem icon={<IoLogOutSharp className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />} label="Logout" />
-        </div>
-      </div>
-      <main className='flex-1'>{children}</main>
-    </div>
-  );
-};
 
-// NavItem component
-const NavItem = ({ icon, label }) => {
-  const handleMouseEnter = (event) => {
-    event.currentTarget.style.color = 'purple';
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const NavItem: React.FC<NavItemProps> = ({ item }) => {
+  const handleMouseEnter = (event: React.MouseEvent) => {
+    (event.currentTarget as any).style.color = 'pink';
   };
 
-  const handleMouseLeave = (event) => {
-    event.currentTarget.style.color = 'blue';
+  const handleMouseLeave = (event: React.MouseEvent) => {
+    (event.currentTarget as any).style.color = 'white';
   };
 
   return (
     <div
-      className='flex items-center text-blue-600 my-1 p-1 rounded-lg navitems'
+      className='flex items-center text-white my-1 p-1 rounded-lg navitems'
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <div className='flex items-end'>
-        <div className='mr-2 mb-10'>{icon}</div>
-        <span className='md:text-xl text-sm mb-10'>{label}</span>
+      <div className='flex items-center justify-center w-10 h-10 mr-2'>{item.icon}</div>
+      <span className='md:text-base text-sm'>{item.label}</span>
+    </div>
+  );
+};
+
+
+export const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  return (
+    <div className='flex h-screen fixed'>
+      <div className='flex flex-col w-30 md:w-60 p-2 sm:p-3 md:p-5 bg-customBlue items-center justify-center'>
+        <div className='flex items-center mb-10'>
+          <img src={'./logo.png'} alt="arrow" className="md:w-12 md:h-12 w-6 h-6 object-cover -ml-1 mb-2 mt-2" />
+          <span className='text-white md:text-2xl sm:text-sm font-bold mb-2 ml-2 mt-2'>RAJO</span>
+        </div>
+        <hr className='mb-5 w-60 mb-10' style={{ borderColor: 'white' }} />
+        <div className='flex flex-col items-start justify-start'>
+          <NavItem item={{ icon: <LuLayoutGrid className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />, label: 'Dashboard' }} />
+          <NavItem item={{ icon: <TbAlertTriangle className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />, label: 'Alerts' }} />
+          <NavItem item={{ icon: <PiUsersThreeDuotone className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />, label: 'CHV' }} />
+          <NavItem item={{ icon: <PiUsersFour className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />, label: 'Children' }} />
+        </div>
+
+        <div className='mt-auto flex flex-col items-start justify-start'>
+          <NavItem item={{ icon: <RiAccountCircleLine className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />, label: 'Profile' }} />
+          <NavItem item={{ icon: <MdLogout className="icon md:w-8 md:h-8 w-4 h-4 -ml-2" />, label: 'Logout' }} />
+        </div>
       </div>
+      <main className='flex-1'>{children}</main>
     </div>
   );
 };
